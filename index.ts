71b87026e01fc2b30c1bf58084f10f72f37a1c00
22emini <emininioluwaakinrinade716@example.com>
2026-06-27 21:28:26 +0100

    refactor: restrict root route to GET requests only

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction): any => {
     next(err);
   });
   
-  app.use('/',(req:Request , res:Response)=>{
+  app.get('/',(req:Request , res:Response)=>{
     res.status(200).json({
         message: "welcome to the storeVerse API made by Eminioluwa"
     })
